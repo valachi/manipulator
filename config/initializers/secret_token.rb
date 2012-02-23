f754dab86f00223ce86f4e1de8d219727d5c395f
valachi <impulse221@yandex.ru>
2012-02-23 20:47:36 +0400

    initial commit with static controller

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Manipulator::Application.config.secret_token = 'f86bc7920b7ce8c0b20a575f04845742d32cf5f1e26d557175636138df2febe758c18da574b929feb47dd365c0c24ebcdb76fa95cf15d6ec93e66180d5333a86'
