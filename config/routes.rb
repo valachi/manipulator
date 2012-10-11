abbebf865dfc64c512cdc472add029b1ffc4a18e
valachi <impulse221@yandex.ru>
2012-10-11 16:43:11 +0400

    Minor fixes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Manipulator::Application.routes.draw do
   root to: 'static#index'
-  match 'info' => 'static#info'
+  # match 'info' => 'static#info'
   match 'prices' => 'static#prices'
   match 'faq' => 'static#faq'
+  match 'contacts' => 'static#contacts'
   # match 'services' => 'static#services'
   match 'gallery' => 'static#gallery'
   post 'order_form/send_email', as: :send_email
